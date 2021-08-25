b697572745aef3cd928c663c262727ad79480ac7
syed <mujtaba1747@gmail.com>
2021-08-26 02:01:06 +0530

    added blog-cosmos

---
+++ blog/vue/src/store/generated/example/blog/example.blog.blog/module/types/blog/query.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export const protobufPackage = 'example.blog.blog';
+export class QueryClientImpl {
+    constructor(rpc) {
+        this.rpc = rpc;
+    }
+}
---
+++ blog/vue/src/store/generated/example/blog/example.blog.blog/module/types/blog/tx.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export const protobufPackage = 'example.blog.blog';
+export class MsgClientImpl {
+    constructor(rpc) {
+        this.rpc = rpc;
+    }
+}
